f3b56f9d66f120645125a0decc8c060869aa1835
hyk029 <164828610+hyk029@users.noreply.github.com>
2025-03-19 17:10:20 -0700

    Updated C181FP.py

--- C181FP.py
+++ C181FP.py
@@ -1,3 +1,4 @@
+#Import necessary packages
 import os
 import argparse
 from typing import Tuple
@@ -10,27 +11,43 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
 
-class CNN(nn.Module):
+# +
+# Smoothing the labels in order to prevent over confidence
+class LabelSmoothingCrossEntropy(nn.Module):
+    def __init__(self, smoothing=0.1):
+        super().__init__()
+        self.smoothing = smoothing
 
-    def __init__(self, num_classes=10, dropout_rate=0.0):
-        super(CNN, self).__init__()
+    def forward(self, predictions, targets):
+        n_classes = predictions.size(1)
+        log_probs = F.log_softmax(predictions, dim=1)
+        with torch.no_grad():
+            true_dist = torch.zeros_like(predictions)
+            true_dist.fill_(self.smoothing / (n_classes - 1))
+            true_dist.scatter_(1, targets.unsqueeze(1), 1.0 - self.smoothing)
+        return torch.mean(torch.sum(-true_dist * log_probs, dim=1))
+
+# +
+#CNN (SimpleCNN) and ResNet18 Architectures 
+class SimpleCNN(nn.Module):
+    def __init__(self, num_classes=10, dropout_rate=0.3):
+        super(SimpleCNN, self).__init__()
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.dropout = nn.Dropout(p=dropout_rate)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(128 * 8 * 8, 256)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.dropout = nn.Dropout(p=dropout_rate)
+        self.fc1 = nn.Linear(128 * 4 * 4, 256)
         self.fc2 = nn.Linear(256, num_classes)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = self.pool(F.relu(self.conv2(x)))
-        x = F.relu(self.conv3(x))
+        x = self.pool(F.relu(self.conv3(x)))
         x = self.dropout(x)
-
-        x = x.view(x.size(0), -1)  
+        x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
-        x = self.dropout(x)        
+        x = self.dropout(x)
         x = self.fc2(x)
         return x
 
@@ -40,63 +57,59 @@ def build_resnet18(num_classes=10):
     model.fc = nn.Linear(model.fc.in_features, num_classes)
     return model
 
+ARCH_MAP = {'simplecnn': SimpleCNN, 'resnet18': build_resnet18,}
 
-ARCH_MAP = {'cnn': CNN, 'resnet18': build_resnet18}
-
-class CIFAR10Loader:
-    def __init__(self, batch_size, advanced_augment=False, num_workers=2):
-        self.batch_size = batch_size
-        self.num_workers = num_workers
-        if advanced_augment:
-            self.train_transform = transforms.Compose([
-                transforms.RandAugment(),
-                transforms.ToTensor(),
-                transforms.Normalize((0.4914, 0.4822, 0.4465),
-                                    (0.2470, 0.2435, 0.2616))
-            ])
-        else:
-            self.train_transform = transforms.Compose([
-                transforms.RandomCrop(32, padding=4),
-                transforms.RandomHorizontalFlip(),
-                transforms.ToTensor(),
-                transforms.Normalize((0.4914, 0.4822, 0.4465),
-                                    (0.2470, 0.2435, 0.2616))
-            ])
-
-        self.test_transform = transforms.Compose([
+# +
+#Load CIFAR-10 and Tiny ImageNet
+def get_cifar10_loaders(batch_size=128, advanced_augment=True):
+    if advanced_augment:
+        train_transform = transforms.Compose([
+            transforms.RandAugment(),  
             transforms.ToTensor(),
             transforms.Normalize((0.4914, 0.4822, 0.4465),
-                                (0.2470, 0.2435, 0.2616))
+                                 (0.2470, 0.2435, 0.2616))
+        ])
+    else:
+        train_transform = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                 (0.2470, 0.2435, 0.2616))
         ])
 
-    def get_data_loaders(self):
-        train_set = torchvision.datasets.CIFAR10(
-            root='./data',
-            train=True,
-            download=True,
-            transform=self.train_transform
-        )
-        val_set = torchvision.datasets.CIFAR10(
-            root='./data',
-            train=False,
-            download=True,
-            transform=self.test_transform
-        )
+    test_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465),
+                             (0.2470, 0.2435, 0.2616))
+    ])
 
-        train_loader = DataLoader(
-            train_set,
-            batch_size=self.batch_size,
-            shuffle=True,
-            num_workers=self.num_workers
-        )
-        val_loader = DataLoader(
-            val_set,
-            batch_size=self.batch_size,
-            shuffle=False,
-            num_workers=self.num_workers
-        )
-        
-        return train_loader, val_loader
+    train_set = torchvision.datasets.CIFAR10(
+        root='./data',
+        train=True,
+        download=True,
+        transform=train_transform
+    )
+    test_set = torchvision.datasets.CIFAR10(
+        root='./data',
+        train=False,
+        download=True,
+        transform=test_transform
+    )
+
+    train_loader = DataLoader(
+        train_set,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=2
+    )
+    test_loader = DataLoader(
+        test_set,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=2
+    )
+    return train_loader, test_loader
 
 
 class TinyImageNetLoader:
@@ -110,7 +123,7 @@ class TinyImageNetLoader:
                 transforms.RandAugment(),
                 transforms.ToTensor(),
                 transforms.Normalize((0.4802, 0.4481, 0.3975),
-                                    (0.2770, 0.2691, 0.2821))
+                                     (0.2770, 0.2691, 0.2821))
             ])
         else:
             self.train_transform = transforms.Compose([
@@ -118,13 +131,13 @@ class TinyImageNetLoader:
                 transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
                 transforms.Normalize((0.4802, 0.4481, 0.3975),
-                                    (0.2770, 0.2691, 0.2821))
+                                     (0.2770, 0.2691, 0.2821))
             ])
 
         self.val_transform = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.4802, 0.4481, 0.3975),
-                                (0.2770, 0.2691, 0.2821))
+                                 (0.2770, 0.2691, 0.2821))
         ])
 
     def _make_dataloader(self, folder, transform, shuffle=True):
@@ -139,11 +152,12 @@ class TinyImageNetLoader:
     def get_data_loaders(self):
         train_folder = os.path.join(self.root, 'train')
         val_folder = os.path.join(self.root, 'val')
-
         train_loader = self._make_dataloader(train_folder, self.train_transform, shuffle=True)
         val_loader = self._make_dataloader(val_folder, self.val_transform, shuffle=False)
         return train_loader, val_loader
 
+# +
+# Training and Evaluating Functions
 def train_one_epoch(model, loader, optimizer, criterion, device):
     model.train()
     running_loss = 0.0
@@ -164,6 +178,7 @@ def evaluate(model, loader, criterion, device):
     running_loss = 0.0
     correct = 0
     total = 0
+
     with torch.no_grad():
         for images, labels in loader:
             images, labels = images.to(device), labels.to(device)
@@ -177,101 +192,91 @@ def evaluate(model, loader, criterion, device):
 
     avg_loss = running_loss / len(loader)
     accuracy = 100.0 * correct / total
-    
     return avg_loss, accuracy
 
+# +
 def main():
+    import argparse
     parser = argparse.ArgumentParser()
-
-    parser.add_argument('--dataset', type=str, default='cifar10',
-                        choices=['cifar10', 'tinyimagenet'],
-                        help='Which dataset to train on.')
-    parser.add_argument('--data_root', type=str, default='./tiny-imagenet-200',
-                        help='Root path for Tiny ImageNet. Ignored for CIFAR-10.')
-    parser.add_argument('--arch', type=str, default='simplecnn',
+    parser.add_argument('--arch', type=str, default='resnet18',
                         choices=['simplecnn', 'resnet18'],
-                        help='Model architecture.')
+                        help='Network architecture')
+    parser.add_argument('--epochs', type=int, default=30,
+                        help='Number of epochs')
     parser.add_argument('--lr', type=float, default=0.001,
-                        help='Learning rate.')
+                        help='Learning rate')
     parser.add_argument('--batch_size', type=int, default=128,
-                        help='Batch size.')
-    parser.add_argument('--epochs', type=int, default=10,
-                        help='Number of training epochs.')
+                        help='Batch size')
     parser.add_argument('--optimizer', type=str, default='adam',
                         choices=['adam', 'sgd'],
-                        help='Which optimizer to use.')
-    parser.add_argument('--dropout_rate', type=float, default=0.0,
-                        help='Dropout rate for SimpleCNN.')
-    parser.add_argument('--label_smoothing', type=float, default=0.0,
-                        help='Label smoothing parameter (0.0 means no smoothing).')
-
-    parser.add_argument('--advanced_augment', action='store_true',
-                        help='Use RandAugment or standard transforms.')
+                        help='Optimizer: adam or sgd')
+    parser.add_argument('--label_smoothing', type=float, default=0.1,
+                        help='Label smoothing factor (0 = off)')
+    parser.add_argument('--dropout_rate', type=float, default=0.3,
+                        help='Dropout rate for SimpleCNN')
     parser.add_argument('--use_cosine_scheduler', action='store_true',
-                        help='Use a Cosine Annealing LR scheduler.')
-    args = parser.parse_args()
+                        help='Use CosineAnnealingLR if set')
+    parser.add_argument('--advanced_augment', action='store_true',
+                        help='Use RandAugment if set')
+    args, unknown = parser.parse_known_args()
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f"Using device: {device}")
 
-    if args.dataset == 'cifar10':
-        loader_builder = CIFAR10Loader(
-            batch_size=args.batch_size,
-            advanced_augment=args.advanced_augment
-        )
-        train_loader, val_loader = loader_builder.get_data_loaders()
-        num_classes = 10
-    else:
-        loader_builder = TinyImageNetLoader(
-            root=args.data_root,
-            batch_size=args.batch_size,
-            advanced_augment=args.advanced_augment
-        )
-        train_loader, val_loader = loader_builder.get_data_loaders()
-        num_classes = 200
+    # Data loaders
+    train_loader, test_loader = get_cifar10_loaders(
+        batch_size=args.batch_size,
+        advanced_augment=args.advanced_augment
+    )
 
-    if args.arch == 'cnn':
-        model = CNN(num_classes=num_classes, dropout_rate=args.dropout_rate)
+    # Models
+    if args.arch == 'simplecnn':
+        model = SimpleCNN(num_classes=10, dropout_rate=args.dropout_rate)
     else:
-        model = build_resnet18(num_classes=num_classes)
+        model = build_resnet18(num_classes=10)
     model.to(device)
 
+    # Loss Function
     if args.label_smoothing > 0:
         criterion = LabelSmoothingCrossEntropy(smoothing=args.label_smoothing)
     else:
         criterion = nn.CrossEntropyLoss()
 
+    # Optimizer
     if args.optimizer == 'adam':
         optimizer = optim.Adam(model.parameters(), lr=args.lr)
     else:
         optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9)
 
+    # Cosine Annealing Learning Rate scheduling
     if args.use_cosine_scheduler:
-        scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.epochs)
+        lrs = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.epochs)
     else:
-        scheduler = None
+        lrs = None
 
-    best_val_acc = 0.0
+    # Training loop
+    best_acc = 0.0
     for epoch in range(args.epochs):
         train_loss = train_one_epoch(model, train_loader, optimizer, criterion, device)
-        val_loss, val_acc = evaluate(model, val_loader, criterion, device)
+        val_loss, val_acc = evaluate(model, test_loader, criterion, device)
 
-        if scheduler:
-            scheduler.step()
+        if lrs:
+            lrs.step() 
 
-        if val_acc > best_val_acc:
-            best_val_acc = val_acc
+        if val_acc > best_acc:
+            best_acc = val_acc
 
-        print(f"Epoch [{epoch+1}/{args.epochs}] "
-            f"Train Loss: {train_loss:.4f}  "
-            f"Val Loss: {val_loss:.4f}  "
-            f"Val Acc: {val_acc:.2f}%  "
-            f"(Best: {best_val_acc:.2f}%)")
+        print(f"Epoch [{epoch+1}/{args.epochs}] | "
+              f"Train Loss: {train_loss:.4f}, "
+              f"Val Loss: {val_loss:.4f}, "
+              f"Val Acc: {val_acc:.2f}% (Best: {best_acc:.2f}%)")
 
-    print("Training Complete")
-    print(f"Best Validation Accuracy: {best_val_acc:.2f}%")
-
+    print("Training complete.")
+    print(f"Best Validation Accuracy: {best_acc:.2f}%")
 
 if __name__ == '__main__':
     main()
+# -
+
+
 
